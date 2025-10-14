9b73d8af17fd27d7063645bf74e6d89edbdf5d4e
Alejazzz <57218838+Alejazzz@users.noreply.github.com>
2025-10-14 10:34:01 -0400

    Update AuthController.java

--- project-overview/AccountService/src/main/java/com/web/controller/AuthController.java
+++ project-overview/AccountService/src/main/java/com/web/controller/AuthController.java
@@ -1,3 +1,38 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.webage.security.TokenService;
+
+@RestController
 public class AuthController {
+
+    //  PART 3a -
+    //  Add code here to define a member variable of type TokenService.
+    //  Annotate this with @Autowired so Spring can dependency inject it:
+    @Autowired
+    TokenService tokenService;
+
+
+    //  PART 3b -
+    //  Add code here to define a method to respond to POST requests on the /token endpoint.
+    //  The method should take one parameter of type Authentication and return a String.
+    //  The method should return the result of a call the tokenService’s generateToken() method:
+
+    @PostMapping("/token")
+    public String token(Authentication authentication) {
+        return tokenService.generateToken(authentication);
+    }
+
+
+
+    @GetMapping("/")
+    public String health() {
+        return "Authorization Server is running.";
+    }
 
 }
